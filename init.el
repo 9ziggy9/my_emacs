d5866fdb7a1217403fb6ddefdb5acb8d5051d943
9ziggy9 <davidarogers@protonmail.com>
2022-03-01 18:41:17 -0500

    ignore appears to be working

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
 ;; PACKAGE MANAGEMENT
 (require 'package)
 
-(setq package-archive '(("melpa" . "https://melpa.org/packages/")
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			("org" . "https://orgmode.org/elpa/")
 			("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
@@ -28,3 +28,16 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
