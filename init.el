53dd21f8ac296304be4d2d65ef3870812b7d0356
Ziggy <davidarogers@protonmail.com>
2022-03-27 14:33:03 +0000

    python support, also debugging although not sure how to use

--- init.el
+++ init.el
@@ -265,13 +265,6 @@
 ;; for react/JSX stuff
 (use-package rjsx-mode)
 
-;; HASKELL mode -- THERE BE DRAGONS HERE FOR ARCH USERS
-(use-package haskell-mode)
-(use-package hindent) ;; haskell indentation help
-
-;; PYTHON
-(use-package pyvenv) ;; python environment support
-
 ;; WEB MODE -- html/css/js
 (use-package web-mode
   :mode ("\\.html\\'")
@@ -288,6 +281,47 @@
          :map js2-mode-map
          ("C-c b" . web-beautify-js)))
 
+;; DEBUGGING
+(use-package dap-mode
+  ;; Uncomment the config below if you want all UI panes to be hidden by default!
+  ;; :custom
+  ;; (lsp-enable-dap-auto-configure nil)
+  ;; :config
+  ;; (dap-ui-mode 1)
+  :commands dap-debug
+  :config
+  ;; Set up Node debugging
+  (require 'dap-node)
+  (dap-node-setup) ;; Automatically installs Node debug adapter if needed
+
+  ;; Bind `C-c l d` to `dap-hydra` for easy access
+  (general-define-key
+    :keymaps 'lsp-mode-map
+    :prefix lsp-keymap-prefix
+    "d" '(dap-hydra t :wk "debugger")))
+
+;; HASKELL mode -- THERE BE DRAGONS HERE FOR ARCH USERS
+(use-package haskell-mode)
+(use-package hindent) ;; haskell indentation help
+
+;; PYTHON
+(use-package python-mode
+  :ensure t
+  :hook (python-mode . lsp-deferred)
+  :custom
+  ;; NOTE: Set these if Python 3 is called "python3" on your system!
+  ;; (python-shell-interpreter "python3")
+  ;; (dap-python-executable "python3")
+  (dap-python-debugger 'debugpy)
+  :config
+  (require 'dap-python))
+
+(use-package pyvenv
+  :after python-mode
+  :config
+  (pyvenv-mode 1))
+
+
 ;;;;;;;; DO NOT TOUCH ;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -298,7 +332,7 @@
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(delete-selection-mode nil)
  '(package-selected-packages
-   '(hindent haskell-mode web-beautify web-mode rjsx-mode fzf js2-mode yasnippet-snippets yasnippet pyvenv python-mode manpages manpges ccls lsp-mode evil-surround wrap-region evil-multiedit hydra evil-mc fixmee autopair multiple-cursors evil-easymotion helpful evil-collection evil general blackboard-theme kooten-theme all-the-icons ivy-rich which-key rainbow-delimiters green-is-the-new-black-theme green-phosphor-theme counsel swiper ivy command-log-mode use-package)))
+   '(dap-mode hindent haskell-mode web-beautify web-mode rjsx-mode fzf js2-mode yasnippet-snippets yasnippet pyvenv python-mode manpages manpges ccls lsp-mode evil-surround wrap-region evil-multiedit hydra evil-mc fixmee autopair multiple-cursors evil-easymotion helpful evil-collection evil general blackboard-theme kooten-theme all-the-icons ivy-rich which-key rainbow-delimiters green-is-the-new-black-theme green-phosphor-theme counsel swiper ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
