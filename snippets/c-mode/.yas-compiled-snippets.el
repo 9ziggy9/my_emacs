13a455dc7c3d8c938f795dca2c2f4e23074386e6
Ziggy <davidarogers@protonmail.com>
2022-03-30 19:53:59 +0000

    snippet changes

---
+++ snippets/c-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `c-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'c-mode
+		     '(("main" "int main(int argc, char** argv) {\n    $1\n    return 0;\n}" "main" nil nil nil "/home/ziggy/.emacs.d/snippets/c-mode/main" nil nil)))
+
+
+;;; Do not edit! File generated at Wed Mar 30 13:07:44 2022
---
+++ snippets/fundamental-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `fundamental-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'fundamental-mode
+		     '(("hello" "HELLO, WORLD!" "hello" nil nil nil "/home/ziggy/.emacs.d/snippets/fundamental-mode/hello" nil nil)))
+
+
+;;; Do not edit! File generated at Wed Mar 30 13:07:44 2022
