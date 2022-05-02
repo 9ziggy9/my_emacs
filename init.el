371d0739ea2f32264af18f27c03cc497952993a1
Ziggy <davidarogers@protonmail.com>
2022-05-02 13:44:29 -0700

    python config is ridiculous, just commenting it out

--- init.el
+++ init.el
@@ -334,21 +334,21 @@
 (use-package hindent) ;; haskell indentation help
 
 ;; PYTHON
-(use-package python-mode
-  :ensure t
-  :hook (python-mode . lsp-deferred)
-  :custom
-  ;; NOTE: Set these if Python 3 is called "python3" on your system!
-  ;; (python-shell-interpreter "python3")
-  ;; (dap-python-executable "python3")
-  (dap-python-debugger 'debugpy)
-  :config
-  (require 'dap-python))
+;; (use-package python-mode
+;;   :ensure t
+;;   :hook (python-mode . lsp-deferred)
+;;   :custom
+;;   ;; NOTE: Set these if Python 3 is called "python3" on your system!
+;;   ;; (python-shell-interpreter "python3")
+;;   ;; (dap-python-executable "python3")
+;;   (dap-python-debugger 'debugpy)
+;;   :config
+;;   (require 'dap-python))
 
-(use-package pyvenv
-  :after python-mode
-  :config
-  (pyvenv-mode 1))
+;; (use-package pyvenv
+;;   :after python-mode
+;;   :config
+;;   (pyvenv-mode 1))
 
 ;; GO
 (use-package go-mode
